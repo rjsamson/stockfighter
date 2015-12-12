afa79c5bd9321b0d2e991d6b747677b40f1fe44d
Robert J Samson <rjsamson@me.com>
2015-12-11 23:34:30 -0500

    Additional GM features

--- lib/stockfighter/gm.rb
+++ lib/stockfighter/gm.rb
@@ -2,24 +2,62 @@ require 'httparty'
 
 module Stockfighter
   class GM
-    def initialize(key)
+    GM_URL = "https://www.stockfighter.io/gm"
+
+    attr_accessor :instance_id
+
+    def initialize(key:, level:)
       @api_key = key
+      @level = level
+
+      resp = HTTParty.post("#{GM_URL}/levels/#{level}", :headers => {"X-Starfighter-Authorization" => @api_key})
+
+      update_config(resp)
     end
 
-    def config_for(level:)
-      resp = HTTParty.post("https://www.stockfighter.io/gm/levels/#{level}", :headers => {"X-Starfighter-Authorization" => @api_key})
-
-      config = {}
-      config[:account] = resp["account"]
-      config[:venue] = resp["venues"][0]
-      config[:symbol] = resp["tickers"][0]
-      config[:key] = @api_key
-
-      if config[:account] && config[:venue] && config[:symbol]
-        config
+    def config
+      if @config[:account] && @config[:venue] && @config[:symbol]
+        @config
       else
         nil
       end
     end
+
+    def restart
+      if @instance_id
+        resp = HTTParty.post("#{GM_URL}/instances/#{@instance_id}/restart", :headers => {"X-Starfighter-Authorization" => @api_key})
+        udpate_config(resp)
+      end
+    end
+
+    def stop
+      if @instance_id
+        resp = HTTParty.post("#{GM_URL}/instances/#{@instance_id}/stop", :headers => {"X-Starfighter-Authorization" => @api_key})
+        update_config(resp)
+      end
+    end
+
+    def resume
+      if @instance_id
+        resp = HTTParty.post("#{GM_URL}/instances/#{@instance_id}/resume", :headers => {"X-Starfighter-Authorization" => @api_key})
+        update_config(resp)
+      end
+    end
+
+    def active?
+      response = HTTParty.get("#{GM_URL}/instances/#{@instance_id}", :headers => {"X-Starfighter-Authorization" => @api_key})
+      response["done"] && response["ok"]
+    end
   end
+
+  def update_config(resp)
+    @config = {}
+    @config[:key] = @api_key
+    @config[:account] = resp["account"]
+    @config[:venue] = resp["venues"][0]
+    @config[:symbol] = resp["tickers"][0]
+
+    @instance_id = resp["instanceId"]
+  end
+  private :update_config
 end
