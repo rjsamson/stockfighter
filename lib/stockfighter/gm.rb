4087e159eb0b318da1f6d696486a11adb9fa7949
Peter Leschev <pleschev@atlassian.com>
2015-12-15 20:43:31 +1100

    Move http request handling into 1 method for gm

--- lib/stockfighter/gm.rb
+++ lib/stockfighter/gm.rb
@@ -10,10 +10,7 @@ module Stockfighter
       @api_key = key
       @level = level
 
-# , :debug_output => $stdout
-      resp = HTTParty.post("#{GM_URL}/levels/#{level}", :headers => {"X-Starfighter-Authorization" => @api_key})
-
-      update_config(resp)
+      perform_request("post", "#{GM_URL}/levels/#{level}")
     end
 
     def config
@@ -25,29 +22,24 @@ module Stockfighter
     end
 
     def restart
-      if @instance_id
-        resp = HTTParty.post("#{GM_URL}/instances/#{@instance_id}/restart", :headers => {"X-Starfighter-Authorization" => @api_key})
-        update_config(resp)
-      end
+      perform_request("post", "#{GM_URL}/instances/#{@instance_id}/restart")
     end
 
     def stop
-      if @instance_id
-        resp = HTTParty.post("#{GM_URL}/instances/#{@instance_id}/stop", :headers => {"X-Starfighter-Authorization" => @api_key})
-        update_config(resp)
-      end
+      perform_request("post", "#{GM_URL}/instances/#{@instance_id}/stop")
     end
 
     def resume
-      if @instance_id
-        resp = HTTParty.post("#{GM_URL}/instances/#{@instance_id}/resume", :headers => {"X-Starfighter-Authorization" => @api_key})
-        update_config(resp)
-      end
+      perform_request("post", "#{GM_URL}/instances/#{@instance_id}/resume")
     end
 
     def active?
       response = get_instance()
       response["done"] && response["ok"]
+    end
+
+    def get_instance
+      perform_request("get", "#{GM_URL}/instances/#{@instance_id}")
     end
 
     def block_until_instance_state(expected_state)
@@ -58,13 +50,13 @@ module Stockfighter
       end
     end
 
-    def get_instance
-      response = HTTParty.get("#{GM_URL}/instances/#{@instance_id}", :headers => {"X-Starfighter-Authorization" => @api_key})
+    def perform_request(action, url)
+      response = HTTParty.method(action).call(url, :headers => {"X-Starfighter-Authorization" => @api_key})
       if response.code != 200
-        raise "HTTP error response received from #{GM_URL}: #{response.code}"
+        raise "HTTP error response received from #{url}: #{response.code}"
       end
       if not response["ok"]
-        raise "Error response received from #{GM_URL}: #{response['error']}"
+        raise "Error response received from #{url}: #{response['error']}"
       end
 
       if response.key?('flash')
@@ -74,36 +66,27 @@ module Stockfighter
           raise "TODO: Unhandled flash scenario: #{response}"
         end
       end
+      if response.key?('instructions')
+        if response['instructions'].key?('Instructions')
+          puts "\e[#34m#{response['instructions']['Instructions']}\e[0m"
+        else
+          raise "TODO: Unhandled instructions scenario: #{response}"
+        end
+      end
+
+      if action == 'post'
+        @config = {}
+        @config[:key] = @api_key
+        @config[:account] = response["account"]
+        @config[:venue] = response["venues"][0]
+        @config[:symbol] = response["tickers"][0]
+
+        @instance_id = response["instanceId"]        
+      end
 
       response
     end
-
-    def update_config(resp)
-
-      if resp.key?('instructions')
-        if resp['instructions'].key?('Instructions')
-          puts "\e[#34m#{resp['instructions']['Instructions']}\e[0m"
-        else
-          raise "TODO: Unhandled instructions scenario: #{resp}"
-        end
-      end
-
-      if resp.code == 200
-        if resp["ok"]
-          @config = {}
-          @config[:key] = @api_key
-          @config[:account] = resp["account"]
-          @config[:venue] = resp["venues"][0]
-          @config[:symbol] = resp["tickers"][0]
-
-          @instance_id = resp["instanceId"]
-        else
-          raise "Error response received from #{GM_URL}: #{resp['error']}"
-        end
-      else
-        raise "HTTP error response received from #{GM_URL}: #{resp.code}"
-      end
-    end
-    private :update_config
+ 
+    private :perform_request
   end
 end
