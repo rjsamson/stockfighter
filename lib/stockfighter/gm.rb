7ed56e40bf126abcc90198a7e09ab8eb3a47f9a6
Peter Leschev <pleschev@atlassian.com>
2015-12-15 20:00:33 +1100

    Enough API changes to allow first_steps to be completed trivially

--- lib/stockfighter/gm.rb
+++ lib/stockfighter/gm.rb
@@ -10,6 +10,7 @@ module Stockfighter
       @api_key = key
       @level = level
 
+# , :debug_output => $stdout
       resp = HTTParty.post("#{GM_URL}/levels/#{level}", :headers => {"X-Starfighter-Authorization" => @api_key})
 
       update_config(resp)
@@ -45,11 +46,47 @@ module Stockfighter
     end
 
     def active?
-      response = HTTParty.get("#{GM_URL}/instances/#{@instance_id}", :headers => {"X-Starfighter-Authorization" => @api_key})
+      response = get_instance()
       response["done"] && response["ok"]
     end
 
+    def block_until_instance_state(expected_state)
+      loop do
+        current_state = get_instance()['state']
+        puts "Waiting for game instance to change - expected_state: #{expected_state}, current_state: #{current_state}"
+        break if current_state == expected_state
+      end
+    end
+
+    def get_instance
+      response = HTTParty.get("#{GM_URL}/instances/#{@instance_id}", :headers => {"X-Starfighter-Authorization" => @api_key})
+      if response.code != 200
+        raise "HTTP error response received from #{GM_URL}: #{response.code}"
+      end
+      if not response["ok"]
+        raise "Error response received from #{GM_URL}: #{response['error']}"
+      end
+
+      if response.key?('flash')
+        if response['flash'].key?('success')
+          puts "\e[#32m#{response['flash']['success']}\e[0m"
+        else
+          raise "TODO: Unhandled flash scenario: #{response}"
+        end
+      end
+
+      response
+    end
+
     def update_config(resp)
+
+      if resp.key?('instructions')
+        if resp['instructions'].key?('Instructions')
+          puts "\e[#34m#{resp['instructions']['Instructions']}\e[0m"
+        else
+          raise "TODO: Unhandled instructions scenario: #{resp}"
+        end
+      end
 
       if resp.code == 200
         if resp["ok"]
