7c65d5cec17f7afe0ead2542f88bf72d5b71bc64
Peter Leschev <pleschev@atlassian.com>
2015-12-18 15:00:14 +1100

    Callbacks for handling of messages that come from gm

--- lib/stockfighter/gm.rb
+++ lib/stockfighter/gm.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'rufus-scheduler'
 
 module Stockfighter
   class GM
@@ -10,7 +11,35 @@ module Stockfighter
       @api_key = key
       @level = level
 
-      perform_request("post", "#{GM_URL}/levels/#{level}")
+      @callback_types = ['success', 'info']
+      @message_callbacks = Hash.new { |h,k| h[k] = [] }
+      @state_change_callback = []
+
+      new_level_response = perform_request("post", "#{GM_URL}/levels/#{level}")
+      previous_state = new_level_response['state']
+
+      # websocket API functionality instead of polling would be great here
+      scheduler = Rufus::Scheduler.new(:overlap => false)
+      scheduler.every '10s' do
+        response = get_instance()
+
+        current_state = response['state']
+        if previous_state != current_state
+          @state_change_callback.each { |callback|
+            callback.call(previous_state, current_state)
+          }
+          previous_state = current_state
+        end
+      end
+    end
+
+    def add_message_callback(type, &block)
+      raise "Unknown message callback type #{type}" unless @callback_types.include? type
+      @message_callbacks[type] << block
+    end
+
+    def add_state_change_callback(&block)
+      @state_change_callback << block
     end
 
     def config
@@ -60,14 +89,20 @@ module Stockfighter
       end
 
       if response.key?('flash')
-        if response['flash'].key?('success')
-          puts "\e[#32m#{response['flash']['success']}\e[0m"
-        elsif response['flash'].key?('info')
-          puts "\e[#34m#{response['flash']['info']}\e[0m"
+        flash = response['flash']
+        if flash.key?('success')
+          @message_callbacks['success'].each { |callback|
+            callback.call(flash['success'])
+          }
+        elsif flash.key?('info')
+          @message_callbacks['info'].each { |callback|
+            callback.call(flash['info'])
+          }
         else
           raise "TODO: Unhandled flash scenario: #{response}"
         end
       end
+
       if response.key?('instructions')
         if response['instructions'].key?('Instructions')
           puts "\e[#34m#{response['instructions']['Instructions']}\e[0m"
--- stockfighter.gemspec
+++ stockfighter.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "httparty", "~> 0.13.7"
+  spec.add_runtime_dependency "rufus-scheduler"
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 end
